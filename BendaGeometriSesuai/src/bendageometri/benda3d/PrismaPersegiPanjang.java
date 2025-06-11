db753e0f029b064f192b2bc2c3ccc94809e30cf6
AidanMahesa <aidanbandjar@gmail.com>
2025-06-11 20:52:00 +0700

    double check

--- BendaGeometriSesuai/src/bendageometri/benda3d/PrismaPersegiPanjang.java
+++ BendaGeometriSesuai/src/bendageometri/benda3d/PrismaPersegiPanjang.java
@@ -9,46 +9,69 @@ import bendageometri.benda2d.PersegiPanjang;
  *
  * @author nbnrc
  */
-public class PrismaPersegiPanjang extends PersegiPanjang { // Mewarisi PersegiPanjang untuk alasnya
-    private double tinggiPrisma; // Bisa mutable dengan setter
-
+public class PrismaPersegiPanjang extends PersegiPanjang {
+    private double volume;
+    private double luasPermukaan;
+    private double tinggiPrisma;
+    
     // Konstruktor utama
-    public PrismaPersegiPanjang(double panjangAlas, double lebarAlas, double tinggiPrisma) {
-        super(panjangAlas, lebarAlas); // Memanggil konstruktor PersegiPanjang
-                                       // Dimensi alas (panjangAlas, lebarAlas) menjadi immutable
-                                       // Validasi panjangAlas dan lebarAlas ditangani oleh PersegiPanjang
-        setTinggiPrisma(tinggiPrisma);   // Menggunakan setter untuk validasi tinggiPrisma
+    public PrismaPersegiPanjang(double panjang, double lebar, double tinggiPrisma) throws IllegalArgumentException {
+        super(panjang, lebar); // Memanggil konstruktor PersegiPanjang
+        
+        if (tinggiPrisma <= 0) {
+            throw new IllegalArgumentException("Tinggi prisma harus bernilai positif.");
+        }
+        
+        this.tinggiPrisma = tinggiPrisma;
+        this.volume = hitungVolume();
+        this.luasPermukaan = hitungLuasPermukaan();
     }
-
-
+    
+    // Getter untuk volume
+    public double getVolume() {
+        return volume;
+    }
+    
+    // Getter untuk luasPermukaan
+    public double getLuasPermukaan() {
+        return luasPermukaan;
+    }
+    
     // Getter untuk tinggiPrisma
     public double getTinggiPrisma() {
         return tinggiPrisma;
     }
-
-    // Setter untuk tinggiPrisma dengan validasi
-    public void setTinggiPrisma(double tinggiPrisma) {
-        if (tinggiPrisma <= 0) {
-            throw new IllegalArgumentException("Tinggi prisma harus bernilai positif.");
-        }
-        this.tinggiPrisma = tinggiPrisma;
-    }
-
-    // Getter untuk panjangAlas (yaitu getPanjang()) dan lebarAlas (yaitu getLebar())
-    // diwarisi dari PersegiPanjang (immutable)
-
+    
     @Override
     public double hitungVolume() {
-        // Volume Prisma = Luas Alas (dari PersegiPanjang) * tinggiPrisma
-        return super.hitungLuas() * this.tinggiPrisma;
+        // Volume Prisma = Luas Alas * tinggiPrisma
+        return this.luas * this.tinggiPrisma;
     }
-
+    
+    // Overloading untuk hitungVolume dengan parameter
+    public double hitungVolume(double panjang, double lebar, double tinggiPrisma) {
+        if (panjang <= 0 || lebar <= 0 || tinggiPrisma <= 0) {
+            throw new IllegalArgumentException("Semua parameter harus bernilai positif.");
+        }
+        double luasAlas = panjang * lebar;
+        return luasAlas * tinggiPrisma;
+    }
+    
     @Override
     public double hitungLuasPermukaan() {
-        // Luas Permukaan Prisma = 2 * Luas Alas + Luas Selimut
-        // Luas Selimut = Keliling Alas * tinggiPrisma
-        double luasAlas = super.hitungLuas();
-        double kelilingAlas = super.hitungKeliling();
+        // Luas Permukaan = 2 * Luas Alas + Luas Selimut
+        double luasAlas = this.luas;
+        double kelilingAlas = this.keliling;
         return (2 * luasAlas) + (kelilingAlas * this.tinggiPrisma);
+    }
+    
+    // Overloading untuk hitungLuasPermukaan dengan parameter
+    public double hitungLuasPermukaan(double panjang, double lebar, double tinggiPrisma) {
+        if (panjang <= 0 || lebar <= 0 || tinggiPrisma <= 0) {
+            throw new IllegalArgumentException("Semua parameter harus bernilai positif.");
+        }
+        double luasAlas = panjang * lebar;
+        double kelilingAlas = 2 * (panjang + lebar);
+        return (2 * luasAlas) + (kelilingAlas * tinggiPrisma);
     }
 }
